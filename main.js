25b36bafb08709bc8aeb756c50046c689ae4de21
IStrikeboomI <shubhmakadia12@gmail.com>
2024-11-20 15:51:55 -0600

    Added dynamic pages to sitemap

--- main.js
+++ main.js
@@ -1,45 +1,83 @@
 const details = document.querySelector("details");
-details.addEventListener("click", (e) => {
-  if (details.hasAttribute("open")) { // since it's not closed yet, it's open!
-    e.preventDefault(); // stop the default behavior, meaning - the hiding
-    details.classList.add("closing"); // add a class which applies the animation in CSS
-  }
-});
+if (details !== null) {
+    details.addEventListener("click", (e) => {
+    if (details.hasAttribute("open")) { // since it's not closed yet, it's open!
+        e.preventDefault(); // stop the default behavior, meaning - the hiding
+        details.classList.add("closing"); // add a class which applies the animation in CSS
+    }
+    });
 
-// when the "close" animation is over
-details.addEventListener("animationend", (e) => {
-  if (e.animationName === "close") {
-    details.removeAttribute("open"); // close the element
-    details.classList.remove("closing"); // remove the animation
-  }
-});
-
+    // when the "close" animation is over
+    details.addEventListener("animationend", (e) => {
+    if (e.animationName === "close") {
+        details.removeAttribute("open"); // close the element
+        details.classList.remove("closing"); // remove the animation
+    }
+    });
+}
+//Allow inline mathjax
 window.MathJax = {
     tex: {
         inlineMath: [['$', '$'], ['\\(', '\\)']]
     }
 };
-fetch('/sitemap.json')
-    .then((response) => response.json())
-    .then((json) => {
-        console.log(json);
-        let pathName = window.location.pathname;
-        //if is notes
-        if (pathName.includes("notes")) {
-            pathName = pathName.substring(7,pathName.length);
-            //class subject like calculus 1
-            let subject = pathName.substring(0,pathName.indexOf("/"));
-            pathName = pathName.substring(subject.length + 1);
-            subject = subject.replaceAll("%20"," ");
-            let topic = pathName.substring(0,pathName.indexOf("/"));
-            pathName = pathName.substring(topic.length + 1);
-            topic = topic.replaceAll("%20"," ");
-            let notesName = pathName.substring(0,pathName.indexOf("."));
-            notesName = notesName.replaceAll("%20"," ");
-            notesName = notesName.replaceAll("%5E","^");
-            
-            console.log(subject);
-            console.log(topic);
-            console.log(notesName);
+let json;
+let subject, topic, notesName;
+
+const request = new XMLHttpRequest();
+request.open("GET", "/sitemap.json", false);
+request.send(null);
+
+if (request.status === 200) {
+    json = JSON.parse(request.responseText);
+    console.log(json);
+    let pathName = window.location.pathname;
+    //if is notes
+    if (pathName.includes("notes")) {
+        pathName = pathName.substring(7,pathName.length);
+        //class subject like calculus 1
+        subject = pathName.substring(0,pathName.indexOf("/"));
+        pathName = pathName.substring(subject.length + 1);
+        subject = subject.replaceAll("%20"," ");
+        topic = pathName.substring(0,pathName.indexOf("/"));
+        pathName = pathName.substring(topic.length + 1);
+        topic = topic.replaceAll("%20"," ");
+        notesName = pathName.substring(0,pathName.indexOf("."));
+        notesName = notesName.replaceAll("%20"," ");
+        notesName = notesName.replaceAll("%5E","^");
+
+        let content = document.getElementById("content");
+        for (let i = 0;i < json.length;i++) {
+            if (json[i].subject.toLowerCase() === subject) {
+                let s = json[i].topics;
+                for (let j = 0;j < s.length;j++) {
+                    if (s[j].section.toLowerCase() === topic) {
+                        let n = s[j].notes;
+                        for (let k = 0;k < n.length;k++) {
+                            if (n[k].name === notesName) {
+                                let header = document.createElement("h2");
+                                header.innerHTML = n[k].title;
+                                content.prepend(header);
+                                console.log(n[k].title);
+                                document.title = n[k].title.replaceAll("$","");
+                            }
+                        }
+                    }
+                }
+            }
         }
-    });
+    }
+    //add to side bar
+    const sidebar = document.getElementById("side-bar");
+    for (let i = 0;i < json.length;i++) {
+        let tr = document.createElement("tr");
+        let th = document.createElement("th");
+        th.classList.add("align-left-cell");
+        let a = document.createElement("a");
+        a.href = "/notes/" + json[i].subject.toLowerCase() + ".html";
+        a.innerHTML = json[i].subject;
+        th.appendChild(a);
+        tr.appendChild(th);
+        sidebar.appendChild(tr);
+    }
+}
---
+++ subject.js
@@ -0,0 +1,49 @@
+//meant to be implemented in the subject home page
+//ex. calculus 1, pre calculus, linear algebra
+//just gets all the files in the sitemap for this subject and adds them
+subject = window.location.pathname;
+subject = subject.substring(subject.lastIndexOf("/") + 1);
+subject = subject.substring(0,subject.length - 5);
+subject = subject.replaceAll("%20"," ");
+console.log(subject);
+const content = document.getElementById("content");
+let title;
+for (let i = 0;i < json.length;i++) {
+    if (json[i].subject.toLowerCase() === subject) {
+        title = json[i].subject;
+        document.title = json[i].subject;
+        let header = document.createElement("h2");
+        header.innerHTML = title;
+        content.appendChild(header);
+        content.appendChild(document.createElement("hr"));
+
+        let topics = json[i].topics;
+        let list = document.createElement("ul");
+        for (let j = 0;j < topics.length;j++) {
+            let li = document.createElement("li");
+            li.classList.add("no-bullets");
+            let details = document.createElement("details");
+            let summary = document.createElement("summary");
+            let h3 = document.createElement("h3");
+            h3.innerHTML = topics[j].section;
+            summary.appendChild(h3);
+            details.appendChild(summary);
+            let notes = topics[j].notes;
+            let notesList = document.createElement("ul");
+            for (let k = 0;k < notes.length;k++) {
+                let notesLi = document.createElement("li");
+                let p = document.createElement("p");
+                let a = document.createElement("a");
+                a.href = "/notes/" + subject + "/" + notes[k].name + ".html";
+                a.innerHTML = notes[k].title;
+                p.appendChild(a);
+                notesLi.appendChild(p);
+                notesList.appendChild(notesLi);
+            }
+            details.appendChild(notesList);
+            li.appendChild(details);
+            content.appendChild(li);
+        }
+        break;
+    }
+}
